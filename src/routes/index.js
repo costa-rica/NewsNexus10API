49c973fa782d5f10a36297d27048270437fb6857
costa-rica <nrodrig1@gmail.com>
2025-12-22 16:28:52 -0800

    log: logging in index page to verify the logging works

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const path = require("path");
+const { logger } = require("../modules/logger");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.sendFile(path.join(__dirname, "../templates/index.html"));
+  console.log("[console] Home page accessed");
+  logger.info("[logger] Home page accessed");
+  res.sendFile(path.join(__dirname, "../templates/index.html"));
 });
 
 module.exports = router;
